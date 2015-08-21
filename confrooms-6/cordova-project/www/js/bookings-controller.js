393f84d1314db65c87233f2f7be7ca5fc05341d2
MiamiCoder <miamicoder@gmail.com>
2015-08-21 10:39:42 -0400

    Adding cordova project and server modules.

---
+++ confrooms-6/cordova-project/www/js/bookings-controller.js
@@ -0,0 +1,50 @@
+﻿var BookIt = BookIt || {};
+
+BookIt.BookingsController = function () {
+    this.localStorageKey = "bookit.bookings";
+    this.$bookingsPage = null;
+    this.$btnRefresh = null;
+};
+
+BookIt.BookingsController.prototype.init = function () {
+    this.$bookingsPage = $("#page-bookings");
+    this.$btnRefresh = $("#btn-refresh", this.$bookingsPage);
+};
+
+BookIt.BookingsController.prototype.getBookingsFromLocalStorage = function () {
+    return window.localStorage.getItem(this.localStorageKey) || [];
+};
+
+BookIt.BookingsController.prototype.showBookings = function () {
+
+    var bookings = this.getBookingsFromLocalStorage();
+
+    // TODO: Build bookings DOM.
+};
+
+BookIt.BookingsController.prototype.getBookingsFromServer = function (successCallback, errorCallback) {
+    var session = BookIt.Session.getInstance().get();
+
+    if (!session) {
+        return errorCallback({ err: BookIt.ApiMessages.SESSION_NOT_FOUND });
+    }
+
+    $.ajax({
+        type: 'GET',
+        url: BookIt.Settings.bookingsUrl + "/sessionId=" + session.sessionId,
+        success: successCallback,
+        error: errorCallback
+    });
+};
+
+BookIt.BookingsController.prototype.onRefreshCommand = function () {
+    
+    this.getBookingsFromServer(
+        function (resp) {
+            // TODO
+        },
+        function (error) {
+            // TODO
+        }
+    );
+};
---
+++ confrooms-6/server/models/booking.js
@@ -0,0 +1,18 @@
+﻿// ./models/booking.js
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var BookingSchema = new Schema({
+    ownerUserId: String,
+    locationId: String,
+    dateTimeFrom: Date,
+    dateTimeTo: Date,
+    numberOfAttendees: Number,
+    needsNetwork: Boolean,
+    needsConfPhone: Boolean,
+    needsVideo: Boolean,
+    needsInternet: Boolean
+});
+
+module.exports = mongoose.model('Booking', BookingSchema);
